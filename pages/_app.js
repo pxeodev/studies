280542cb54d7391665a3bf09f98eba719953e84a
Sascha Mayr <mayr.sascha@gmail.com>
2021-10-15 13:29:58 +0200

    Fix HTML structure

--- pages/_app.js
+++ pages/_app.js
@@ -6,13 +6,12 @@ import Container from 'react-bootstrap/Container'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <html lang="en">
+    <>
       <Head>
         <title>Supertrend Matrix</title>
         <meta name="description" content="Daily Supertrend values for the top 100 crypto currency tokens."/>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <body>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
             <Navbar.Brand href="#home">Supertrend Matrix</Navbar.Brand>
@@ -22,8 +21,7 @@ function MyApp({ Component, pageProps }) {
           </Container>
         </Navbar>
         <Component {...pageProps} className="mt-5" />
-      </body>
-    </html>
+    </>
   )
 }
 
