e771cceb1edb6662b7f785acd5909182d77d1987
Sascha Mayr <mayr.sascha@gmail.com>
2022-03-14 14:32:42 +0000

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,28 @@
 import "../styles/ant.less"
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from "react";
 
 import useDarkMode from '../hooks/usedarkmode'
 import DarkModeSwitch from '../components/DarkModeSwitch'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
   const [darkMode, setDarkMode] = useDarkMode();
   const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   const currentUrl = `${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`
   pageProps.currentUrl = currentUrl
 
@@ -17,6 +30,23 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `,
+        }}
+      />
       <Head>
         <title key="title">CoinRotator</title>
         <meta name="description" key="description" content="Coinrotator screens the top 1000 crypto coins daily for fresh signals. Always be on the right side of the market."/>
