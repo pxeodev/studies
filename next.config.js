85a198cf73ba7933819df2917b54221c9acf48ea
Sascha Mayr <mayr.sascha@gmail.com>
2022-04-13 08:03:07 +0100

    Install Sentry for the frontend

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withLess = require("next-with-less");
 const { withSuperjson } = require('next-superjson')
+const { withSentryConfig } = require('@sentry/nextjs');
 
-
-module.exports = withLess(withSuperjson()({
+let moduleExports = {
   lessLoaderOptions: {
     /* ... */
   },
@@ -14,4 +14,18 @@ module.exports = withLess(withSuperjson()({
   env: {
     'TZ': 'UTC',
   },
-}));
+}
+moduleExports = withLess(withSuperjson()(moduleExports))
+moduleExports = withSentryConfig(moduleExports, {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+})
+
+module.exports = moduleExports;
