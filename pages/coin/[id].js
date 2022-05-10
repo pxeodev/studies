ee606f03a1eecfb716fe951202edaa5afd47d476
Sascha Mayr <mayr.sascha@gmail.com>
2022-05-10 20:17:48 +0200

    Add market cap rank again

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -444,6 +444,7 @@ export async function getStaticProps({ params }) {
     'categories',
     'defaultPlatform',
     'marketCap',
+    'marketCapRank',
     'ath',
     'atl',
     'fullyDilutedValuation',
