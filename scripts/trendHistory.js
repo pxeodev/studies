0bf70d171cb5e53b638b514da51dea58590f396f
Sascha Mayr <mayr.sascha@gmail.com>
2022-10-18 18:23:29 +0200

    Refactor the trend history script

--- scripts/trendHistory.js
+++ scripts/trendHistory.js
@@ -1,6 +1,4 @@
 import dotenv from 'dotenv';
-import { init, startTransaction, captureException } from '@sentry/node';
-import * as Tracing from '@sentry/tracing'
 import endOfYesterday from 'date-fns/endOfYesterday';
 import subDays from 'date-fns/subDays'
 import format from 'date-fns/format'
@@ -16,14 +14,6 @@ import supersupertrend from '../utils/supersupertrend';
 import { signals } from '../utils/variables';
 
 dotenv.config();
-init({
-  dsn: process.env.SENTRY_DSN,
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
 const compileTrendHistory = async () => {
   const coinsData = await prisma.coin.findMany({
@@ -35,7 +25,7 @@ const compileTrendHistory = async () => {
   })
   const trendDays = [];
   for (const coinData of coinsData) {
-    console.log(`Fetching ${coinData.symbol} data`)
+    console.log(`Fetching ${coinData.name} (${coinData.symbol.toUpperCase()}) data`)
     let ohlcs = await prisma.ohlc.findMany({
       select: {
         closeTime: true,
@@ -55,7 +45,10 @@ const compileTrendHistory = async () => {
     })
     ohlcs = convertToDailySignals(ohlcs)
     const trends = mapValues(ohlcs, (ohlcs) => {
-      return supertrend(ohlcs, { atrPeriods: defaultAtrPeriods, multiplier: defaultMultiplier })
+      return supertrend(ohlcs, {
+        atrPeriods: defaultAtrPeriods,
+        multiplier: defaultMultiplier
+      })
     })
     const dailySuperSuperTrends = []
     let reverseUsdTrends = trends.usd?.reverse() || [];
@@ -72,59 +65,43 @@ const compileTrendHistory = async () => {
         date,
         superSupertrend
       })
-      if (!trendDays.find((existingDay) => existingDay.getTime() === date.getTime())) {
+      const dayAlreadyStored = trendDays.find((existingDay) => existingDay.getTime() === date.getTime())
+      if (!dayAlreadyStored) {
         trendDays.push(date);
       }
     }
 
     coinData.dailySuperSuperTrends = dailySuperSuperTrends;
   }
-  const superTrendsByDay = trendDays.map((day) => {
+  const trendReportFile = await fs.open('./supertrends.csv', 'a+');
+  for (let date of trendDays) {
     const upCoins = coinsData.filter((coin) => {
       return coin.dailySuperSuperTrends.find((dailyTrend) => {
-        return dailyTrend.date.getTime() === day.getTime() && dailyTrend.superSupertrend === signals.buy
+        return dailyTrend.date.getTime() === date.getTime() &&
+               dailyTrend.superSupertrend === signals.buy
       })
     })
     const hodlCoins = coinsData.filter((coin) => {
       return coin.dailySuperSuperTrends.find((dailyTrend) => {
-        return dailyTrend.date.getTime() === day.getTime() && dailyTrend.superSupertrend === signals.hodl
+        return dailyTrend.date.getTime() === date.getTime() &&
+               dailyTrend.superSupertrend === signals.hodl
       })
     })
     const downCoins = coinsData.filter((coin) => {
       return coin.dailySuperSuperTrends.find((dailyTrend) => {
-        return dailyTrend.date.getTime() === day.getTime() && dailyTrend.superSupertrend === signals.sell
+        return dailyTrend.date.getTime() === date.getTime() &&
+               dailyTrend.superSupertrend === signals.sell
       })
     })
-    return {
-      day,
-      [signals.buy]: upCoins.map(coin => coin.symbol),
-      [signals.hodl]: hodlCoins.map(coin => coin.symbol),
-      [signals.sell]: downCoins.map(coin => coin.symbol),
-    }
-  })
-  const file = await fs.open('./supertrends.csv', 'a+');
-  superTrendsByDay.forEach((day) => {
+
     const data = [
-      format(day.day, 'yyyy-MM-dd'),
-      day[signals.buy].length,
-      day[signals.hodl].length,
-      day[signals.sell].length
+      format(date, 'yyyy-MM-dd'),
+      upCoins.length,
+      hodlCoins.length,
+      downCoins.length,
     ]
-    file.writeFile(`${data.join(',')}\n`)
-  })
+    trendReportFile.writeFile(`${data.join(',')}\n`)
+  }
 }
 
-setTimeout(async () => {
-  const transaction = startTransaction({
-    op: "compileTrendHistory",
-    name: "compileTrendHistory",
-  });
-  try {
-    await compileTrendHistory()
-  } catch (e) {
-    captureException(e);
-    console.log(e)
-  } finally {
-    transaction.finish();
-  }
-}, 99);
+compileTrendHistory();
