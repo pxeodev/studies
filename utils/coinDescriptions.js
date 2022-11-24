2e3fc8e4960bad99c561ef218b8e21cd2b6b4a15
Sascha Mayr <mayr.sascha@gmail.com>
2022-11-24 17:22:45 +0100

    Revise coin template

--- utils/coinDescriptions.js
+++ utils/coinDescriptions.js
@@ -9,7 +9,7 @@ function addTemplateDescription(description, coin) {
 
 ${coin.marketCap ? 'As of {{month}} {{day}} {{year}} {{name}} has a marketcap of {{marketcap}}. ' : ''}
 ${coin.ath ? 'This is {{percentagefromath}} from its all time high of {{ath}}. ' : ''}
-${coin.totalSupply ? 'In terms of its tokenomics, with a total supply of {{totalsupply}} with {{percentagecirculatingsupply}} outstanding. ' : ''}
+${coin.totalSupply ? `In terms of its tokenomics, there's a total supply of {{totalsupply}} with {{percentagecirculatingsupply}} currently outstanding. ` : ''}
 ${coin.fullyDilutedValuation ? 'Keep in mind {{name}} has a fully diluted value of {{fdv}} which many investors might interpret as overvalued. ' : ''}
 ${coin.launch_price ? 'Be wary as insiders are still up from its launch price of {{launchprice}}. ' : ''}
 Of course, don’t trust price predictions alone, always check the Coinrotator token screener to follow the trending market.`
