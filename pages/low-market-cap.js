9060641af2bcb2035dce1a9548f1534c65cc7a14
Sascha Mayr <mayr.sascha@gmail.com>
2023-02-01 13:07:11 +0700

    Low market cap coins

--- pages/low-market-cap.js
+++ pages/low-market-cap.js
@@ -1,42 +1,136 @@
-import { Layout, Card } from 'antd';
+import { Layout, Row } from 'antd';
 import endOfYesterday from 'date-fns/endOfYesterday';
-import { useContext } from 'react'
-import subDays from 'date-fns/subDays';
-import isEqualDate from 'date-fns/isEqual';
 import subWeeks from 'date-fns/subWeeks';
 
-import { DarkModeContext } from './_app';
 import baseStyles from '../styles/base.module.less'
+import indexStyles from '../styles/index.module.less'
 import globalData from '../lib/globalData';
 import PageHeader from '../components/PageHeader'
-import { signals, defaultAtrPeriods, defaultMultiplier } from '../utils/variables.mjs'
-import useBreakPoint from '../hooks/useBreakPoint';
+import TableFilters from '../components/TableFilters'
+import CoinTable from '../components/CoinTable';
+import { defaultAtrPeriods, defaultMultiplier } from '../utils/variables.mjs'
 import convertToDailySignals from '../utils/convertToDailySignals';
+import convertTickersToExchanges from '../utils/convertTickersToExchanges';
 import getTrends from '../utils/getTrends.mjs'
+import useTableFilters from '../hooks/useTableFilters';
 
-const { Content } = Layout;
-
-export default function MarketHealth({  }) {
-  const [darkMode] = useContext(DarkModeContext);
-  const screens = useBreakPoint();
+export default function LowMarketCap({ coinsData, appData, exchangeData }) {
+  const [formState, formDispatch, defaultFormState, portfolioInputValue, setPortfolioInputValue] = useTableFilters(coinsData)
   return (
     <>
       <PageHeader title="Low market cap coins" />
-      <Content className={baseStyles.container}>
-        <Card>
-
-        </Card>
-      </Content>
+      <Layout.Content className={baseStyles.container}>
+        <TableFilters
+          coinsData={coinsData}
+          categories={appData.categories}
+          portfolioInputValue={portfolioInputValue}
+          formState={formState}
+          formDispatch={formDispatch}
+          defaultFormState={defaultFormState}
+          setPortfolioInputValue={setPortfolioInputValue}
+        />
+        <Row className={indexStyles.tableRow}>
+          <CoinTable
+            coinsData={coinsData}
+            exchangeData={exchangeData}
+            marketCapMax={formState.marketCapMax}
+            marketCapMin={formState.marketCapMin}
+            trendLengthMin={formState.trendLengthMin}
+            trendLengthMax={formState.trendLengthMax}
+            portfolio={formState.portfolio}
+            category={formState.category}
+            trendType={formState.trendType}
+            defaultCategory={defaultFormState.category}
+            exchanges={formState.exchanges}
+            derivatives={formState.derivatives}
+            superTrendFlavor={formState.superTrendFlavor}
+          />
+        </Row>
+      </Layout.Content>
     </>
   );
 }
 
 export async function getStaticProps() {
   const appData = await globalData();
+  const yesterday = endOfYesterday();
+  const coinQuery = {
+    orderBy: { marketCapRank: 'asc' },
+    select: {
+      id: true,
+      symbol: true,
+      name: true,
+      images: true,
+      marketCap: true,
+      marketCapRank: true,
+      categories: true,
+      tickers: true,
+      derivatives: true,
+      ohlcs: {
+        select: {
+          closeTime: true,
+          open: true,
+          high: true,
+          low: true,
+          close: true,
+          quoteSymbol: true
+        },
+        where: {
+          closeTime: {
+            lte: yesterday,
+            gte: subWeeks(yesterday, 12)
+          }
+        },
+        orderBy: { closeTime: 'asc' }
+      }
+    },
+    where: {
+      marketCap: {
+        lte: 100000000
+      }
+    }
+  }
+  let coinsData
+  if (process.env.NODE_ENV === 'development') {
+    coinsData = await prisma.coin.findMany({...coinQuery, take: 20})
+  } else {
+    coinsData = await prisma.coin.findMany({...coinQuery, take: 1000})
+  }
+  coinsData = coinsData.map((coinData) => {
+    const ohlcs = convertToDailySignals(coinData.ohlcs)
+    const [dailyTrends, dailySuperSuperTrend] = getTrends(ohlcs, defaultAtrPeriods, defaultMultiplier, false)
+    const [weeklyTrends, weeklySuperSuperTrend] = getTrends(ohlcs, defaultAtrPeriods, defaultMultiplier, true)
+    const [dailyClassicTrends, dailyClassicSuperSuperTrend] = getTrends(ohlcs, 10, 3, false)
+    const [weeklyClassicTrends, weeklyClassicSuperSuperTrend] = getTrends(ohlcs, 10, 3, true)
+    delete coinData.ohlcs
 
+    const exchanges = convertTickersToExchanges(coinData.tickers)
+    delete coinData.tickers
+
+    return {
+      ...coinData,
+      dailyTrends,
+      dailySuperSuperTrend,
+      weeklyTrends,
+      weeklySuperSuperTrend,
+      dailyClassicTrends,
+      dailyClassicSuperSuperTrend,
+      weeklyClassicTrends,
+      weeklyClassicSuperSuperTrend,
+      ath: Number(coinData.ath),
+      atl: Number(coinData.atl),
+      fullyDilutedValue: Number(coinData.fullyDilutedValue),
+      circulatingSupply: Number(coinData.circulatingSupply),
+      totalSupply: Number(coinData.totalSupply),
+      exchanges
+    }
+  })
+  const exchangeData = await prisma.exchange.findMany()
   return {
     props: {
-      appData,
+      coinsData,
+      exchangeData,
+      appData
     }
-  };
+  }
 }
