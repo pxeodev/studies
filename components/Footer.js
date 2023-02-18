7f36e731306904270c6b47a2afe0ab4f65c53f04
Sascha Mayr <mayr.sascha@gmail.com>
2023-02-18 13:04:29 +0100

    Remove kick.com

--- components/Footer.js
+++ components/Footer.js
@@ -209,7 +209,7 @@ const Footer = ({ topCoins, topCategories }) => {
         {grid}
       </div>
       <Typography.Paragraph className={footerStyles.footerSecondary} type="secondary">
-        Proudly funded by <a href="https://gamblersarea.com/" target="_blank" rel="noreferrer">GamblersArea</a> and <a href="https://kick.com/" target="_blank" rel="noreferrer">Kick</a>
+        Proudly funded by <a href="https://gamblersarea.com/" target="_blank" rel="noreferrer">GamblersArea</a>
       </Typography.Paragraph>
     </AntFooter>
   );
