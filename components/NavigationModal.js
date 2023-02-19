f7da0f5720d8f3faa7c36e12aebb64e7c6f71ef8
Sascha Mayr <mayr.sascha@gmail.com>
2023-02-19 23:02:04 +0100

    Make the navigation modal closeable

--- components/NavigationModal.js
+++ components/NavigationModal.js
@@ -1,6 +1,6 @@
 import { Modal } from 'antd'
 import { useState } from 'react'
-import { MenuOutlined } from '@ant-design/icons'
+import { MenuOutlined, CloseOutlined } from '@ant-design/icons'
 
 import navigationStyles from '../styles/navigation.module.less'
 import NavigationMenu from './NavigationMenu'
@@ -9,16 +9,19 @@ import Funders from './Funders'
 
 const NavigationModal = ({ topCategories }) => {
   const [navigationVisible, setNavigationVisible] = useState(false)
+  const NavigationIcon = navigationVisible ? CloseOutlined : MenuOutlined
   return (
     <>
-      <span classnames={navigationStyles.hamburgerButton}>
-        <MenuOutlined onClick={() => setNavigationVisible(true)} />
+      <span className={navigationStyles.hamburgerButton}>
+        <NavigationIcon onClick={() => setNavigationVisible(!navigationVisible)} />
       </span>
       <Modal
         open={navigationVisible}
         onCancel={() => setNavigationVisible(false)}
         className={navigationStyles.modal}
         footer={null}
+        mask={false}
+        closable={false}
       >
         <NavigationMenu topCategories={topCategories} />
         <Socials />
