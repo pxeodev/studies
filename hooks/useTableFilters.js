3bebdb3e917ae32f10e5869ab6fef6704b87c67f
Sascha Mayr <mayr.sascha@gmail.com>
2023-03-03 18:42:53 +0100

    Reset the filters after page change

--- hooks/useTableFilters.js
+++ hooks/useTableFilters.js
@@ -198,20 +198,19 @@ const useTableFilters = (coinsData, showDerivativesByDefault = false) => {
       }
     })
   }, [router.isReady, router.query])
-  // useEffect(() => {
-  //   const handleRouteChange = (_url, { shallow }) => {
-  //     console.log('changed', _url, shallow)
-  //     if (!shallow) {
-  //       formDispatch({ type: 'RESET' })
-  //     }
-  //   }
+  useEffect(() => {
+    const handleRouteChange = (_url, { shallow }) => {
+      if (!shallow) {
+        setTimeout(() => formDispatch({ type: 'RESET' }), 500)
+      }
+    }
 
-  //   router.events.on('routeChangeStart', handleRouteChange)
+    router.events.on('routeChangeComplete', handleRouteChange)
 
-  //   return () => {
-  //     router.events.off('routeChangeStart', handleRouteChange)
-  //   }
-  // }, [])
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const setPortfolioDebounced = useCallback(debounce((portfolio) => {
     formDispatch({ type: 'SET_PORTFOLIO', payload: portfolio })
