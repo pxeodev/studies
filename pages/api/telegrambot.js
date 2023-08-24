5ae18aa2d83e7323edb74d9e982b872615cd90a0
Sascha Mayr <mayr.sascha@gmail.com>
2023-08-24 18:32:55 +0200

    First version of Telegram API

---
+++ pages/api/telegrambot.js
@@ -0,0 +1,58 @@
+import subDays from 'date-fns/subDays';
+import pick from 'lodash/pick';
+import groupBy from 'lodash/groupBy';
+import mapKeys from 'lodash/mapKeys';
+import mapValues from 'lodash/mapValues';
+
+import prisma from '../../lib/prisma.mjs'
+import mode from '../../utils/mode.js'
+
+const handler = async (req, res) => {
+  if (req.query.apiKey !== 'TELEGRAM_BOT_API_KEY') {
+    res.status(401).send("Unauthorized")
+  } else if (req.method !== 'GET') {
+    res.status(400).send("Bad request")
+  } else {
+    let coins = await prisma.coin.findMany({
+      orderBy: { marketCapRank: 'asc' },
+      include: {
+        superTrends: {
+          select: {
+            trend: true,
+            quoteSymbol: true,
+            date: true,
+          },
+          where: {
+            flavor: 'CoinRotator',
+            weekly: false,
+            date: {
+              // TODO: Which dates?
+              gte: subDays(new Date(), 10),
+            }
+          },
+          // orderBy: { date: 'asc' },
+        }
+      }
+    })
+    coins = coins.map(coin => {
+      coin = pick(coin, ['symbol', 'name', 'superTrends'])
+      coin.trends = groupBy(coin.superTrends, 'date')
+      delete coin.superTrends
+      coin.trends = mapKeys(coin.trends, (_trends, date) => {
+        date = new Date(date)
+        return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+      })
+      coin.trends = mapValues(coin.trends, (trends, _date) => {
+        return {
+          superTrends: trends.map(superTrend => pick(superTrend, ['trend', 'quoteSymbol'])),
+          superSuperTrend: mode(trends.map(superTrend => superTrend.trend)),
+        }
+      })
+
+      return coin
+    })
+    res.status(200).json({ coins })
+  }
+}
+
+export default handler
