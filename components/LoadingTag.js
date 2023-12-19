5999f21d4eea7928a009e40f74f58d14059f0932
Sascha Mayr <mayr.sascha@gmail.com>
2023-12-19 13:46:52 +0700

    Better tag loading animation

--- components/LoadingTag.js
+++ components/LoadingTag.js
@@ -1,14 +1,7 @@
-import { Tag } from 'antd'
-import classnames from 'classnames'
+import { Skeleton } from 'antd'
 
-import variableStyles from '../styles/variables.module.less'
-import signalStyles from '../styles/signalTag.module.less'
-import { useContext } from 'react'
-import { DarkModeContext } from '../layouts/screener.js'
-
-const LoadingTag = ({ className = '' }) => {
-  const [darkMode] = useContext(DarkModeContext);
-  return <Tag className={classnames(signalStyles.tag, className)} color={variableStyles.crGray9} style={{ color: darkMode ? variableStyles.black : variableStyles.white }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Tag>
+const LoadingTag = () => {
+  return <Skeleton.Button active size="small" />
 }
 
 export default LoadingTag
