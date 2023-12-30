608979206e7aadc3bb71eeccb3352f01fb55dafa
Sascha Mayr <mayr.sascha@gmail.com>
2023-12-30 14:16:00 +0700

    Improvements

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -1,20 +1,35 @@
-import { Layout, Typography } from 'antd';
+import { Layout, Typography, Breadcrumb } from 'antd';
 import { gql } from '@urql/core'
 import Head from 'next/head'
 import Link from 'next/link'
 import fs from 'fs/promises'
 import startCase from 'lodash/startCase'
 import slugify from 'slugify';
+import classnames from 'classnames';
 
-import baseStyles from '../styles/base.module.less'
+import sitemapStyles from '../styles/sitemap.module.less'
 import globalData from '../lib/globalData';
 import PageHeader from '../components/PageHeader'
 import prisma from "../lib/prisma.mjs";
 import strapi from '../utils/strapi';
 
 const { Title } = Typography;
+const EXCLUDED_PAGES = ['_app.js', '_document.js', '_error.js', 'sitemap.js', '404.js', '500.js']
+const SHALLOW_PAGES_FILTER = (page) =>
+  page.endsWith('.js') &&
+  !page.startsWith('_') &&
+  !EXCLUDED_PAGES.includes(page)
+const HUMAN_PAGE_NAMES_EXCEPTIONS = {
+  '4h-alerts': '4H Alerts',
+  'index': 'Home'
+}
+const HUMANIZE_PAGE_NAMES = page => HUMAN_PAGE_NAMES_EXCEPTIONS[page] || startCase(page)
+const APPLICATION_PAGES = ['terms', 'faq', 'watchlist']
 
-export default function BinanceFuturesScreener({ coinsData, appData, pageData, shallowPages }) {
+export default function Sitemap({ coinsData, appData, pageData, shallowPages }) {
+  const breadCrumbItemClass = classnames('ant-breadcrumb-link', sitemapStyles.breadcrumb)
+  const informationPages = shallowPages.filter(page => !APPLICATION_PAGES.includes(page))
+  const applicationPages = shallowPages.filter(page => APPLICATION_PAGES.includes(page))
   return (
     <>
       <Head>
@@ -22,19 +37,29 @@ export default function BinanceFuturesScreener({ coinsData, appData, pageData, s
         <meta name="description" key="description" content={pageData.metaDescription}/>
       </Head>
       <PageHeader lastUpdated={appData.lastUpdated} title={pageData.title} explainer={pageData.content}/>
-      <Layout.Content className={baseStyles.container}>
-        <Title level={2} className={baseStyles.title}>Pages</Title>
-        {shallowPages.map(page =>
-          <Link href={`/${page}`} key={page}>{startCase(page)}</Link>
+      <Layout.Content className={sitemapStyles.container}>
+        <Breadcrumb separator="|" className={sitemapStyles.breadcrumbs}>
+          <Breadcrumb.Item href="#information" className={breadCrumbItemClass}>Information</Breadcrumb.Item>
+          <Breadcrumb.Item href="#application" className={breadCrumbItemClass}>Application</Breadcrumb.Item>
+          <Breadcrumb.Item href="#coins" className={breadCrumbItemClass}>Coins</Breadcrumb.Item>
+          <Breadcrumb.Item href="#categories" className={breadCrumbItemClass}>Categories</Breadcrumb.Item>
+        </Breadcrumb>
+        <Title id="information" level={2} className={sitemapStyles.title}>Information</Title>
+        {informationPages.map(page =>
+          <Link prefetch={false} className={sitemapStyles.link} href={`/${page}`} key={page}>{HUMANIZE_PAGE_NAMES(page)}</Link>
         )}
-        <Link href={`/categories`} key="categories">Categories</Link>
-        <Title level={2} className={baseStyles.title}>Coins</Title>
+        <Title id="application" level={2} className={sitemapStyles.title}>Application</Title>
+        {applicationPages.map(page =>
+          <Link prefetch={false} className={sitemapStyles.link} href={`/${page}`} key={page}>{HUMANIZE_PAGE_NAMES(page)}</Link>
+        )}
+
+        <Title id="coins"  level={2} className={sitemapStyles.title}>Coins</Title>
         {coinsData.map(coin =>
-          <Link href={`/coin/${coin.id}`} key={coin.id}>{coin.name}</Link>
+          <Link prefetch={false} className={sitemapStyles.link} href={`/coin/${coin.id}`} key={coin.id}>{coin.name}</Link>
         )}
-        <Title level={2} className={baseStyles.title}>Categories</Title>
+        <Title id="categories" level={2} className={sitemapStyles.title}>Categories</Title>
         {appData.categories.map(categoryName =>
-          <Link href={`/category/${slugify(categoryName)}`} key={categoryName}>{categoryName}</Link>
+          <Link prefetch={false} className={sitemapStyles.link} href={`/category/${slugify(categoryName)}`} key={categoryName}>{categoryName}</Link>
         )}
       </Layout.Content>
     </>
@@ -44,9 +69,7 @@ export default function BinanceFuturesScreener({ coinsData, appData, pageData, s
 export async function getStaticProps() {
   const appData = await globalData();
   let shallowPages = await fs.readdir('./pages')
-  shallowPages = shallowPages.filter(page =>
-    page.endsWith('.js') && page !== 'sitemap.js' && !page.startsWith('_')
-  ).map(page => page.slice(0, -3))
+  shallowPages = shallowPages.filter(SHALLOW_PAGES_FILTER).map(page => page.slice(0, -3))
 
   let { data } = await strapi.query(
     gql`
