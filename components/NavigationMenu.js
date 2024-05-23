0250dd297bfd6d46005895cdb59eb2b804fd63f1
Sascha Mayr <mayr.sascha@gmail.com>
2024-05-23 10:33:24 +0200

    Rephrasing more datapoints

--- components/NavigationMenu.js
+++ components/NavigationMenu.js
@@ -223,7 +223,7 @@ const NavigationMenu = ({ collapsed = false , topCategories, onMenuItemSelected 
           key: 'lowertimeframes'
         },
         {
-          label: 'More datapoints',
+          label: 'Advanced data',
           icon: <a target="_blank" href="https://coinrotator-git-more-data-points-my-team-49a155bf.vercel.app/"><LinkOutlined className={styles.daybreakBlue} /></a>,
           key: 'moredatapoints'
         },
