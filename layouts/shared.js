f481d12ea223a74c5c95e88140d155ea06286d5f
Sascha Mayr <mayr.sascha@gmail.com>
2024-11-07 11:21:40 +0100

    Install Hotjar

--- layouts/shared.js
+++ layouts/shared.js
@@ -35,6 +35,24 @@ export default function SharedLayout({ pageProps }) {
       }}
     />
   ) : <></>
+  const hotjar = process.env.NODE_ENV === 'production' ? (
+    <Script
+      id="hotjar"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:5199035,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `,
+      }}
+    />
+  ) : <></>
   return (
     <>
       <Head>
@@ -43,6 +61,7 @@ export default function SharedLayout({ pageProps }) {
       </Head>
       {googleAnalytics}
       {mixPanel}
+      {hotjar}
     </>
   )
 }
