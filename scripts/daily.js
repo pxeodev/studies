aaa83656f6f66c396144dae0a0d618f23b12577a
Sascha Mayr <mayr.sascha@gmail.com>
2024-11-18 14:34:41 +0700

    Trigger the netlify main branch daily

--- scripts/daily.js
+++ scripts/daily.js
@@ -306,6 +306,9 @@ setTimeout(async () => {
     await axios.post('https://coinrotator-realtime-fra.onrender.com/new-trends')
     setTimeout(async () => {
       await axios.get('https://api.vercel.com/v1/integrations/deploy/prj_uc9CaXrUEpspFxIJeoTgrrWqaIAY/zigJ5zntts')
+      try {
+        await axios.post('https://api.netlify.com/build_hooks/673aebced624ef0d703c7049')
+      } catch(e) {}
       await createJob({ serviceId: 'crn-c8q7r2pg7hp6tkba3sj0', startCommand: 'node dist/bot.mjs' })
       try {
         await axios.post('https://websocket-pr-6.onrender.com/new-trends')
