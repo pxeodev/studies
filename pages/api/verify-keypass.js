29a2191463e2d9dd490c24409e19060ab106de83
Sascha Mayr <mayr.sascha@gmail.com>
2024-12-29 12:57:44 +0700

    Fix wallet connect for netlify

--- pages/api/verify-keypass.js
+++ pages/api/verify-keypass.js
@@ -19,8 +19,8 @@ const handler = async (req, res) => {
   const walletAddress = req.query.walletAddress
   const validWallet = walletAddress?.startsWith('0x')
   const isLocal = req.headers?.referer?.includes('localhost:300')
-  const isPreview = req.headers?.referer?.includes(process.env.VERCEL_BRANCH_URL)
-  const isProd = req.headers?.referer?.includes('coinrotator.app')
+  const isPreview = req.headers?.referer?.includes(process.env.VERCEL_BRANCH_URL) || req.headers?.host?.includes(process.env.DEPLOY_URL)
+  const isProd = req.headers?.referer?.includes('coinrotator.app') || req.headers?.host?.includes('coinrotator.app')
   const isCoinrotatorReferrer = isLocal || isPreview || isProd
   console.dir(req.headers, { depth: null })
   console.log(walletAddress, validWallet, isLocal, isPreview, isProd)
