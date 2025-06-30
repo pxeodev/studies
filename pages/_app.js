b809e2ed9a72b288c55d672c144a4504548dbaef
coinrotatordev <info@coinrotator.app>
2025-06-30 18:38:01 +0200

    Add bugsnag to frontend code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import { useRouter } from 'next/router'
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
 
 import ScreenerLayout from "../layouts/screener"
 import "../styles/ant.less"
+
+// Only start Bugsnag if it hasn't been started yet (prevents double initialization)
+if (!(Bugsnag._client)) {
+  Bugsnag.start({
+    apiKey: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY,
+    plugins: [new BugsnagPluginReact()]
+  })
+}
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
